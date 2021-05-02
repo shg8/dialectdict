d35279f1ef77de5877e5eb395ddf7fdc55e25642
shg8 <stevengao2018@outlook.com>
2021-05-02 02:37:16 -0400

    search bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -41,14 +41,15 @@
 
             </div>
 
-                <div class="invisible lg:visible self-center pt-2 mb-2 relative mx-auto text-gray-600">
-                    <input
-                        class="search-bar border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                        type="search" id="search-bar-top" placeholder="Search">
-                    <button type="button" class="absolute right-0 top-0 mt-5 mr-4" onclick="search($('#search-bar-top').val())">
-                        <x-heroicon-o-search class="text-gray-600 h-4 w-4 "/>
-                    </button>
-                    @push('scripts')
+            <div class="hidden lg:visible self-center pt-2 mb-2 relative mx-auto text-gray-600">
+                <input
+                    class="search-bar border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                    type="search" id="search-bar-top" placeholder="Search">
+                <button type="button" class="absolute right-0 top-0 mt-5 mr-4"
+                        onclick="search($('#search-bar-top').val())">
+                    <x-heroicon-o-search class="text-gray-600 h-4 w-4 "/>
+                </button>
+                @push('scripts')
                     <script>
                         $('.search-bar').on('keyup', function (e) {
                             if (e.key === 'Enter' || e.keyCode === 13) {
@@ -62,60 +63,60 @@
                             }
                         }
                     </script>
-                    @endpush
-                </div>
-            @if(!Auth::guest())
-                <!-- Settings Dropdown -->
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button
-                                    class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                    <div>{{ Auth::user()->name }}</div>
+                @endpush
+            </div>
+        @if(!Auth::guest())
+            <!-- Settings Dropdown -->
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <div>{{ Auth::user()->name }}</div>
 
-                                    <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                             viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                  clip-rule="evenodd"/>
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
+                                <div class="ml-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                         viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"/>
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-                            <x-slot name="content">
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
+                        <x-slot name="content">
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
 
-                                    <x-dropdown-link :href="route('logout')"
-                                                     onclick="event.preventDefault();
+                                <x-dropdown-link :href="route('logout')"
+                                                 onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                        {{ __('Log out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
+                                    {{ __('Log out') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+            @else
+                <div class="flex flex-row">
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <button
+                            class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <a href="{{ route('register') }}">Register</a>
+                        </button>
                     </div>
-                @else
-                    <div class="flex flex-row">
-                        <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <button
-                                class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <a href="{{ route('register') }}">Register</a>
-                            </button>
-                        </div>
-                        <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <button
-                                class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                                <a href="{{ route('login') }}">Log In</a>
-                            </button>
-                        </div>
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <button
+                            class="flex items-center text-md font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <a href="{{ route('login') }}">Log In</a>
+                        </button>
                     </div>
-                @endif
+                </div>
+        @endif
 
-            <!-- Hamburger -->
+        <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open"
                         class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
